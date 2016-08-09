3729c1a6c60623befa1817f92f050234d49dbd00
Alexander <Alexander@wangrenpeide-macbook-air.local>
2016-08-09 16:05:17 +0800

    half complete tipPro

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func caculatiTip(sender: AnyObject) {
+        guard let billAmount = Double(billAmountField.text!) else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+        
+        var tipPercentage = 0.0
+        
+        switch tipSelector.selectedSegmentIndex {
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.20
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(100*billAmount)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(100*tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+    }
 }
 
